1954111962385b6127774a888394ba29aebd069e
siddarth181717 <sonisiddarth890@gmail.com>
2026-08-21 00:58:18 +0530

    done

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
    ======================================================== */
 
 // Real Supabase Production REST Credentials
-const SUPABASE_URL = 'https://hxtowatfbxckcaswfwzk.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4dG93YXRmYnhja2Nhc3dmd3prIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM4MjY2OTEsImV4cCI6MjA5OTQwMjY5MX0.4I5-TbOEPMctAiMLAKRrwfVr3XvhRtMTBnZ-TAt6zJk';
+const SUPABASE_URL = 'https://viygsuifghscibcmfkqh.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpeWdzdWlmZ2hzY2libWNtZmtxaCIsInJvbGUiOiJhbm9uIiwiaWF0IjoxNzgzNDc4MjAwLCJleHAiOjIwOTkwNTQyMDB9.yiJn50e6MxBz51N3AafBXqTKWPGSwikOBOCWhOkjiDg';
 
 let supabaseClient = null;
 
